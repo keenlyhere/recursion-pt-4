ac7a703daec9985d4e8e93b4a8f44edd0ed77b31
Keenly <keenlyhere@gmail.com>
2022-10-11 16:58:19 -0700

    first commit

---
+++ problems/15-subsets.js
@@ -0,0 +1,38 @@
+/***********************************************************************
+Write a function called `subsets` that will return all subsets of an array.
+
+Examples:
+
+subsets([]) // [[]]
+subsets([1]) // [[], [1]]
+subsets([1, 2]) // [[], [1], [2], [1, 2]]
+subsets([1, 2, 3]) // [[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]]
+
+Hint: For subsets([1, 2, 3]), there are two kinds of subsets:
+  1. Those that do not contain 3 (all of these are subsets of [1, 2]).
+  2. For every subset that does not contain 3, there is also a corresponding
+     subset that is the same, except it also does contain 3.
+***********************************************************************/
+
+const subsets = (arr, depth = 0, subset = [], results = []) => {
+  // base
+  if (depth === arr.length) {
+    results.push(subset);
+  } else {
+    subsets(arr, depth + 1, subset, results);
+    subsets(arr, depth + 1, [...subset, arr[depth]], results)
+  }
+
+  return results
+}
+console.log(subsets([])) // [[]]
+console.log(subsets([1])) // [[], [1]]
+console.log(subsets([1, 2])) // [[], [1], [2], [1, 2]]
+console.log(subsets([1, 2, 3])) // [[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3
+
+/**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
+try {
+  module.exports = subsets;
+} catch (e) {
+  module.exports = null;
+}
